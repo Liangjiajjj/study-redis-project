0dd307fa8bebb73f47bb5cc27717ac0826f838ee
Liangjiajun <qq1017941200>
2021-01-07 21:59:32 +0800

    test

--- seckill/src/main/java/com/sr/scekill/controller/SeckillController.java
+++ seckill/src/main/java/com/sr/scekill/controller/SeckillController.java
@@ -4,13 +4,11 @@ import com.sr.commons.model.domain.ResultInfo;
 import com.sr.commons.model.pojo.SeckillVouchers;
 import com.sr.commons.utils.ResultInfoUtil;
 import com.sr.scekill.service.SeckillService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @RestController
 public class SeckillController {
@@ -23,6 +21,27 @@ public class SeckillController {
     public ResultInfo<String> addSeckillVouchers(@RequestBody SeckillVouchers seckillVouchers) {
         seckillService.addSeckillVouchers(seckillVouchers);
         return ResultInfoUtil.buildSuccess(request.getServletPath(), "添加成功");
+    }
+
+    @PostMapping("{voucherId}")
+    public ResultInfo<String> doSeckill(@PathVariable Integer voucherId,  int userId) {
+        ResultInfo resultInfo = seckillService.doSeckill(voucherId, userId, request.getServletPath());
+        return resultInfo;
+    }
+
+    @PostMapping("test")
+    public void test(){
+        SeckillVouchers seckillVouchers = new SeckillVouchers();
+        seckillService.addSeckillVouchers(seckillVouchers);
+        seckillVouchers.setUpdateDate(new Date());
+        seckillVouchers.setCreateDate(new Date());
+        seckillVouchers.setIsValid(1);
+        seckillVouchers.setAmount(10);
+        seckillVouchers.setId(1);
+        seckillService.addSeckillVouchers(seckillVouchers);
+        for (int i = 0; i < 100; i++) {
+
+        }
     }
 
 }
--- seckill/src/main/java/com/sr/scekill/service/SeckillService.java
+++ seckill/src/main/java/com/sr/scekill/service/SeckillService.java
@@ -2,14 +2,17 @@ package com.sr.scekill.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.IdUtil;
+import com.sr.commons.constant.RedisKeyConstant;
 import com.sr.commons.model.domain.ResultInfo;
 import com.sr.commons.model.pojo.SeckillVouchers;
 import com.sr.commons.model.pojo.VoucherOrders;
 import com.sr.commons.model.vo.SignInDinerInfo;
 import com.sr.commons.utils.AssertUtil;
 import com.sr.commons.utils.ResultInfoUtil;
+import com.sr.scekill.config.RedisTemplateConfiguration;
 import com.sr.scekill.mapper.SeckillVouchersMapper;
 import com.sr.scekill.mapper.VoucherOrdersMapper;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 import java.util.Date;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Service
 public class SeckillService {
@@ -27,9 +31,11 @@ public class SeckillService {
     private VoucherOrdersMapper voucherOrdersMapper;
     @Resource
     private RestTemplate restTemplate;
+    @Resource
+    private RedisTemplate redisTemplate;
 
-    @Transactional(rollbackFor = Exception.class) // 事务是否回退
     // 添加代金券
+    @Transactional(rollbackFor = Exception.class) // 事务是否回退
     public void addSeckillVouchers(SeckillVouchers seckillVouchers) {
         AssertUtil.isNotNull(seckillVouchers, "代金券实体为空");
         AssertUtil.isTrue(seckillVouchers.getFkVoucherId() == 0, "请输入需要抢购的代金券");
@@ -47,9 +53,9 @@ public class SeckillService {
         seckillVouchersMapper.save(seckillVouchers);
     }
 
-
     // 客户端抢代金券（基于数据库）
-    public ResultInfo doSeckillOfMySQL(Integer voucherId, int userId, String path) {
+    @Transactional(rollbackFor = Exception.class) // 事务是否回退
+    public ResultInfo doSeckill(Integer voucherId, int userId, String path) {
         // 基本参数校验
         AssertUtil.isTrue(voucherId == null || voucherId < 0, "请选择需要抢购的代金券");
         // 判断此代金券是否加入抢购
@@ -70,6 +76,74 @@ public class SeckillService {
         // 扣库存
         int count = seckillVouchersMapper.stockDecrease(seckillVouchers.getId());
         AssertUtil.isTrue(count == 0, "该券已经卖完了");
+        // 下单
+        VoucherOrders voucherOrders = new VoucherOrders();
+        voucherOrders.setFkDinerId(userId);
+        voucherOrders.setFkSeckillId(seckillVouchers.getId());
+        voucherOrders.setFkVoucherId(seckillVouchers.getFkVoucherId());
+        String orderNo = IdUtil.getSnowflake(1, 1).nextIdStr();
+        voucherOrders.setOrderNo(orderNo);
+        voucherOrders.setOrderType(1);
+        voucherOrders.setStatus(0);
+        count = voucherOrdersMapper.save(voucherOrders);
+        AssertUtil.isTrue(count == 0, "用户抢购失败");
+        return ResultInfoUtil.buildSuccess(path, "抢购成功");
+    }
+
+
+    // 添加代金券
+    @Transactional(rollbackFor = Exception.class) // 事务是否回退
+    public void addSeckillVouchersOfRedis(SeckillVouchers seckillVouchers) {
+        AssertUtil.isNotNull(seckillVouchers, "代金券实体为空");
+        AssertUtil.isTrue(seckillVouchers.getFkVoucherId() == 0, "请输入需要抢购的代金券");
+        AssertUtil.isTrue(seckillVouchers.getAmount() <= 0, "请输入抢购总数量");
+        Date now = new Date();
+        AssertUtil.isNotNull(seckillVouchers.getStartTime(), "请输入开始时间");
+        AssertUtil.isNotNull(seckillVouchers.getEndTime(), "请输入结束时间");
+        // AssertUtil.isTrue(now.after(seckillVouchers.getStartTime()), "开始时间不能早于当前时间");
+        AssertUtil.isTrue(now.after(seckillVouchers.getEndTime()), "结束时间不能早于当前时间");
+        AssertUtil.isTrue(seckillVouchers.getStartTime().after(seckillVouchers.getEndTime()), "开始时间不能早于结束时间");
+
+        // 是否已经有秒杀活动
+        String redisKey = RedisKeyConstant.seckill_vouchers.getKey() + seckillVouchers.getFkVoucherId();
+        Map<String, Object> seckillVoucherMaps = redisTemplate.opsForHash().entries(redisKey);
+        AssertUtil.isTrue(!seckillVoucherMaps.isEmpty() && (int) seckillVoucherMaps.get("amount") > 0, "该卷已经拥有了活动");
+
+        // 同步到redis上面
+        seckillVouchers.setIsValid(1);
+        seckillVouchers.setCreateDate(new Date());
+        seckillVouchers.setUpdateDate(new Date());
+        seckillVoucherMaps = BeanUtil.beanToMap(seckillVouchers);
+        redisTemplate.opsForHash().putAll(redisKey, seckillVoucherMaps);
+    }
+
+    // 客户端抢代金券（基于Redis）
+    @Transactional(rollbackFor = Exception.class) // 事务是否回退
+    public ResultInfo doSeckillOfRedis(Integer voucherId, int userId, String path) {
+        // 基本参数校验
+        AssertUtil.isTrue(voucherId == null || voucherId < 0, "请选择需要抢购的代金券");
+        // 判断此代金券是否加入抢购
+        String redisKey = RedisKeyConstant.seckill_vouchers.getKey() + voucherId;
+        Map<String, Object> seckillVoucherMaps = redisTemplate.opsForHash().entries(redisKey);
+        SeckillVouchers seckillVouchers = BeanUtil.mapToBean(seckillVoucherMaps, SeckillVouchers.class, true, null);
+
+        AssertUtil.isTrue(seckillVouchers == null, "该代金券并未有抢购活动");
+        // 判断是否有效
+        AssertUtil.isTrue(seckillVouchers.getIsValid() == 0, "该活动已结束");
+        // 判断是否开始、结束
+        Date now = new Date();
+        AssertUtil.isTrue(now.before(seckillVouchers.getStartTime()), "该抢购还未开始");
+        AssertUtil.isTrue(now.after(seckillVouchers.getEndTime()), "该抢购已结束");
+        // 判断是否卖完
+        AssertUtil.isTrue(seckillVouchers.getAmount() < 1, "该券已经卖完了");
+        // 判断登录用户是否已抢到(一个用户针对这次活动只能买一次)
+        VoucherOrders order = voucherOrdersMapper.findDinerOrder(userId,
+                seckillVouchers.getId());
+        AssertUtil.isTrue(order != null, "该用户已抢到该代金券，无需再抢");
+        // 扣库存，使用redis
+        // ----------采用 Redis 解问题----------
+        long count = redisTemplate.opsForHash().increment(redisKey, "amount", -1);
+        AssertUtil.isTrue(count < 0, "该券已经卖完了");
         // 下单
         VoucherOrders voucherOrders = new VoucherOrders();
         voucherOrders.setFkDinerId(userId);
